b39053d808baab84ce2b654e92b78eed134dd8eb
Mariusz12345 <przybylowski303@gmail.com>
2015-08-10 14:23:41 +0200

    PierwszyHQL

--- PierwszyHQL/src/test/hql/HibernateTest.java
+++ PierwszyHQL/src/test/hql/HibernateTest.java
@@ -8,14 +8,16 @@ public class HibernateTest {
 
 	public static void main(String[] args) {
 		UzytkownikDane dane = new UzytkownikDane();
-		dane.setUserID(2);
-		dane.setUserName("janek");
+		dane.setUserID(1);
+		dane.setUserName("cos");
 		
 		SessionFactory sessionfactory = new Configuration().configure().buildSessionFactory();
 		Session session = sessionfactory.openSession();
 		session.beginTransaction();
 		session.save(dane);
 		session.getTransaction().commit();
+		session.close();
+		sessionfactory.close();
 	}
 
 }
